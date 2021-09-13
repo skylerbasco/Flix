8e6c91d02248111f003a5c1f608278bb1283ccdf
Skyler Basco <skylerbasco@gmail.com>
2021-09-13 00:22:58 -0400

    Initial Commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Skyler on 9/12/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
